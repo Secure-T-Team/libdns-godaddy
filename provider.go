2f1666575f122b4d976e69f7e60ff2ed48f0f7c6
Yegor <h3rmit@protonmail.com>
2021-04-05 16:50:55 +0300

    Trim zone

--- provider.go
+++ provider.go
@@ -24,9 +24,8 @@ func getDomain(zone string) string {
 }
 
 func getRecordName(zone, name string) string {
-    fmt.Println("function getRecordName")
-	fmt.Printf("name %v, zone %v", name, zone)
-	return strings.TrimSuffix(strings.TrimSuffix(name, zone), ".")
+	trimmedZone := strings.TrimSuffix(zone, ".")
+	return strings.TrimSuffix(strings.TrimSuffix(name, trimmedZone), ".")
 }
 
 func (p *Provider) getApiHost() string {
